0774423a3b5c59ec25637008165bfd87c4019a28
Raisan <yuri@rainyan.net>
2026-07-23 10:02:13 +0900

    動画部分の改良 ライセンスの追加 OSS表記追加

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 export default {
   SITE_NAME: "WorldCup 2026 Unofficial Japan Funsite",
-  SITE_NAME_JA: "ワールドカップ2026 非公式日本ファンサイト"
+  SITE_NAME_JA: "ワールドカップ2026 非公式日本ファンサイト",
+  GITHUB_URL: "https://github.com/code-raisan/WorldCup2026JapanFunsite"
 };
