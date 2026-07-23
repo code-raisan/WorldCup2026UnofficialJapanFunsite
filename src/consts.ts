54b3d57dc049f8015f36e34cc75de7b497d0f2a3
Raisan <yuri@rainyan.net>
2026-07-23 10:11:36 +0900

    Meta設定

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 export default {
   SITE_NAME: "WorldCup 2026 Unofficial Japan Funsite",
   SITE_NAME_JA: "ワールドカップ2026 非公式日本ファンサイト",
-  GITHUB_URL: "https://github.com/code-raisan/WorldCup2026JapanFunsite"
+  GITHUB_URL: "https://github.com/code-raisan/WorldCup2026JapanFunsite",
+  DESCRIPTION: "このサイトは2026年 FIFAワールドカップの日本語によるファンサイトで、個人的な感想をまとめています。※個人的な主観によって書かれており、誤りがある場合があることをご留意ください。"
 };
